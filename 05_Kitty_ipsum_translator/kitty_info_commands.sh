3a89251cdf582edd319e699a86498f7c19f08381
Immah <emmanuelkaringi@gmail.com>
2022-09-27 10:14:12 +0300

    added kitty_ipsum_translator

---
+++ 05_Kitty_ipsum_translator/kitty_info_commands.sh
@@ -0,0 +1,16 @@
+echo "~~ kitty_ipsum_1.txt info ~~" > kitty_info.txt
+
+echo -e "\nNumber of lines:" >> kitty_info.txt
+cat kitty_ipsum_1.txt | wc -l >> kitty_info.txt
+
+echo -e "\nNumber of words:" >> kitty_info.txt
+cat kitty_ipsum_1.txt | wc -w >> kitty_info.txt
+
+echo -e "\nNumber of characters:" >> kitty_info.txt
+wc -m < kitty_ipsum_1.txt >> kitty_info.txt
+
+echo -e "\nNumber of times meow or meowzer appears:" >> kitty_info.txt
+grep -o 'meow[a-z]*' kitty_ipsum_1.txt | wc -l >> kitty_info.txt
+
+echo -e "\nLines that they appear on:" >> kitty_info.txt
+grep -n 'meow[a-z]*' kitty_ipsum_1.txt | sed -E 's/([0-9]+).*/\1/' >> kitty_info.txt
