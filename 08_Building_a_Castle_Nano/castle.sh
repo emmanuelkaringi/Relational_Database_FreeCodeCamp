ee54655717f9ec0eeffefb01a8dbce5a739f774c
Immah <emmanuelkaringi@gmail.com>
2022-09-29 12:27:10 +0300

    added castle.sh nano program

---
+++ 08_Building_a_Castle_Nano/castle.sh
@@ -0,0 +1,18 @@
+echo "
+Welcome to my castle
+"
+
+echo "
+ /\                /\ 
+/  \              /  \ 
+|  |______________|  |
+|                    |
+|   []    []    []   |
+|                    |
+|         __         |
+|________|  |________|
+"
+
+
+# Modify file by running nano castle.sh in the terminal.
+# Run bash castle.sh in the terminal to run this file.
